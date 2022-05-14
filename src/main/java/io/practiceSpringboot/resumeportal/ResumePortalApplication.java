3778a4347d5bdc05f6ac665da1163f097d314954
meenakshi-subudhi-dodax <m.subudhi@dodax.com>
2022-05-14 14:03:45 +0200

    Initial commit

---
+++ src/main/java/io/practiceSpringboot/resumeportal/ResumePortalApplication.java
@@ -0,0 +1,14 @@
+package io.practiceSpringboot.resumeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumePortalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ResumePortalApplication.class, args);
+		System.out.println("Hello world 123");
+	}
+}
