463845e1ab82a014674a20e4e646ef6ce931d4fe
meenakshi-subudhi-dodax <m.subudhi@dodax.com>
2022-05-14 19:13:16 +0200

    Adding user JPA support for Spring Security

---
+++ src/main/java/io/practiceSpringboot/resumeportal/HomeController.java
@@ -0,0 +1,17 @@
+package io.practiceSpringboot.resumeportal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  @GetMapping("/")
+  public String home()  {
+    return "Hello World";
+  }
+
+  @GetMapping("/edit")
+  public String edit()  {
+    return "Hello World Edit";
+  }
+}
---
+++ src/main/java/io/practiceSpringboot/resumeportal/MyUserDetailsService.java
@@ -0,0 +1,21 @@
+package io.practiceSpringboot.resumeportal;
+
+import io.practiceSpringboot.resumeportal.model.MyUserDetails;
+import io.practiceSpringboot.resumeportal.model.User;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+  @Override public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<User> user = userRepository.findByUserName(username);
+    user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+    return user.map(MyUserDetails::new).get();
+    }
+}
